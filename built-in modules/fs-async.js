c047f2fad1ca0c431cb6f27c9c4d7859b6555d19
Antonio Lega <atlcontact8@gmail.com>
2023-04-26 21:25:59 +0200

    first commit

---
+++ built-in modules/fs-async.js
@@ -0,0 +1,30 @@
+// setup fs async built-in module
+
+const { readFile, writeFile } = require('fs')
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+
+  const first = result
+  readFile('./content/second.txt', 'utf8', (err, result) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    const second = result
+    writeFile(
+      './content/result-async.txt',
+      `Here is the result : ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        console.log(result)
+      }
+    )
+  })
+})
